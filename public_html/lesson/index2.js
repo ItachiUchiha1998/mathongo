42964d80059b4a830f4bb7ecf7bb97e786ed0717
ItachiUchiha1998 <vinayak.shrivastava@gmail.com>
2017-09-27 20:03:48 +0530

    disqus integration-4

--- public_html/lesson/index2.js
+++ public_html/lesson/index2.js
@@ -2,8 +2,8 @@ var iden1 = document.getElementById('nameOfTheTeacher').innerHTML;
 var iden2 = document.getElementById('nameOfTheMiniCourse').innerHTML;
 var iden3 = document.getElementById('name').innerHTML;
 var disqus_config = function () {
-        this.page.url = "http://localhost:8080"+iden1 + iden2 + iden3 ;//+ iden1 + iden2 + iden3 ;
-        this.page.identifier = "123" + iden1 + iden2 + iden3;
+        this.page.url = "http://localhost:8080/"+iden1 +"/" + iden2 +"/" + iden3;
+        this.page.identifier = iden1 + iden2 + iden3;
 };
 (function() {
         var d = document, s = d.createElement('script');
